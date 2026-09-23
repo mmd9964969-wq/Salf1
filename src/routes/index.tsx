61323dcb1bff3dfad47146cf93950f0adef82d8b
mmd9964969-wq <mmd9964969@gmail.com>
2026-09-23 12:38:55 +0330

    reset: clear project for fresh rebuild

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { NexaApp } from "@/components/nexa/app";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <NexaApp />;
-}
